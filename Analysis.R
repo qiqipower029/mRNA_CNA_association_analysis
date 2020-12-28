380f874bcd2a1eef9a3bb633b131a7b59b7743ce
qiqipower029 <tjqjasmine1029@gmail.com>
2020-12-28 15:06:11 -0600

    import data for HNSC

--- Analysis.R
+++ Analysis.R
@@ -2,3 +2,11 @@
 #--------------------------Jieqi Tu--------------------------------------#
 
 
+
+#---------------------HNSC dataset---------------------------#
+# Tidy data
+library(tidyverse)
+CNA_HNSC = read.delim("./Downloaded Data/CNA HNSC.txt")
+mRNA_HNSC = read.delim("./Downloaded Data/mRNA expression z-scores HNSC.txt")
+HNSC = cbind.data.frame(CNA_HNSC, mRNA_HNSC) 
+HNSC = HNSC[-c(5,6)]
